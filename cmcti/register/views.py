f902bc90f33eb5f79a3719c85f3cd07aa0af387a
Lucca Torrezilha Soares <luccatsoares@gmail.com>
2023-04-03 23:04:14 -0300

    change login username to email

--- cmcti/register/views.py
+++ cmcti/register/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+
 
 # Create your views here.
 
 def loginView(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        senha = request.POST['senha']
+        
     return render(request, 'pages/login.html')
