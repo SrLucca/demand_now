d90a12e14df1f53408554b83256b9946b6fb405e
Lucca Torrezilha Soares <luccatsoares@gmail.com>
2023-04-17 16:28:24 -0300

    modelo da demanda

--- cmcti/home/models.py
+++ cmcti/home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Demanda(models.Model):
+    titulo = models.CharField(max_length=300, blank=False, null=False)
+    descricao = models.TextField(blank=False, null=False)
+    prazo = models.DateField(blank=True)
+    criado_por = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
