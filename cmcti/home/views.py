3cfb10644a3916a5fe5c3a1c84a3ebcacc6380c8
Lucca Torrezilha Soares <luccatsoares@gmail.com>
2023-04-17 16:28:01 -0300

    cadastro da demanda

--- cmcti/home/views.py
+++ cmcti/home/views.py
@@ -3,6 +3,11 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-@login_required(login_url='/login')
 def homeView(request):
+    if request.method == 'POST':
+        titulo = request.POST['titulo']
+        descricao = request.POST['descricao']
+        data = request.POST['data']
+
+        print(titulo, descricao, data)
     return render(request, 'pages/home.html')
