47b5d57b672a4899337da512132b59240e757be3
Lucca Torrezilha Soares <luccatsoares@gmail.com>
2023-04-18 10:25:11 -0300

    Criacao do objeto demanda

--- cmcti/home/views.py
+++ cmcti/home/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from home.models import Demanda
+from django.contrib.auth import get_user_model
 
 # Create your views here.
 
 def homeView(request):
     if request.method == 'POST':
-        titulo = request.POST['titulo']
-        descricao = request.POST['descricao']
-        data = request.POST['data']
+        user_model = get_user_model()
+        user = user_model.objects.get(email=request.user.email)
+        data = ""
+        titulo = str(request.POST['titulo'])
+        descricao = str(request.POST['descricao'])
+        if request.POST['data']:
+            data = str(request.POST['data'])
 
-        print(titulo, descricao, data)
+        Demanda.objects.create(titulo=titulo, descricao=descricao, prazo=data, criado_por=user)
+
     return render(request, 'pages/home.html')
