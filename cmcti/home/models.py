58ce036f8962c6819b327540c6a9fbe886486f38
Lucca Torrezilha Soares <luccatsoares@gmail.com>
2023-04-20 15:38:53 -0300

    documento de demanda ou de uso geral

--- cmcti/home/models.py
+++ cmcti/home/models.py
@@ -7,7 +7,7 @@ class Demanda(models.Model):
     titulo = models.CharField(max_length=300, blank=False, null=False)
     descricao = models.TextField(blank=False, null=False)
     prazo = models.DateField(blank=True)
-    documento = models.FileField(blank=True, upload_to='documentos')
+    documento = models.FileField(blank=True, upload_to='documentos/', null=True)
     criado_por = models.ManyToManyField(CustomUser)
 
     def __str__(self):
