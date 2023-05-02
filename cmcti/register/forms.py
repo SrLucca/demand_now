119159663e39a74bcf09648a9ae48ee38eef4c24
Lucca Torrezilha Soares <luccatsoares@gmail.com>
2023-05-02 12:16:26 -0300

    classe form-control nos campos

--- cmcti/register/forms.py
+++ cmcti/register/forms.py
@@ -6,8 +6,11 @@ class CustomAuthenticationForm(AuthenticationForm):
     email = forms.EmailField(widget=forms.TextInput(attrs={'autofocus': True}))
 
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-    last_name = forms.CharField(label='Sobrenome')
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class':"form-control"}))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class':"form-control"}))
+    last_name = forms.CharField(label='Sobrenome', widget=forms.TextInput(attrs={'class':"form-control"}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':"form-control"}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':"form-control"}))
     class Meta:
         model = CustomUser
         fields = ('email', 'first_name', 'last_name' ,'password1', 'password2')
