c115a7591844f04fadb97590799ded9812fff436
Lucca Torrezilha Soares <luccatsoares@gmail.com>
2023-05-05 16:03:10 -0300

    Update forms.py

--- cmcti/register/forms.py
+++ cmcti/register/forms.py
@@ -9,7 +9,7 @@ class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class':"form-control"}))
     first_name = forms.CharField(widget=forms.TextInput(attrs={'class':"form-control"}))
     last_name = forms.CharField(label='Sobrenome', widget=forms.TextInput(attrs={'class':"form-control"}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':"form-control"}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':"form-control", 'aria-describedby':"passwordHelpBlock"}))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':"form-control"}))
     class Meta:
         model = CustomUser
