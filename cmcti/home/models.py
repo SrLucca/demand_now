9db25aecebe80a2e7a79cde91cab068d81baba08
Lucca Torrezilha Soares <luccatsoares@gmail.com>
2023-05-29 18:21:11 -0300

    tipo de cadastro

--- cmcti/home/models.py
+++ cmcti/home/models.py
@@ -2,9 +2,16 @@ from django.db import models
 from register.models import CustomUser
 
 # Create your models here.
+TIPOS = (
+    ('Demanda','Demanda'),
+    ('Documento','Documento')
+    )
+    
+
 
 class Demanda(models.Model):
     titulo = models.CharField(max_length=300, blank=False, null=False)
+    tipo = models.CharField(choices=TIPOS, blank=False, null=False, max_length=50)
     descricao = models.TextField(blank=False, null=False)
     prazo = models.DateField(blank=True)
     documento = models.FileField(blank=True, upload_to='documentos/', null=True)
