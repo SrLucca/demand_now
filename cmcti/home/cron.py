9a3ab130daa4e3896d067e6967162113cd5cfe93
Lucca Torrezilha Soares <luccatsoares@gmail.com>
2023-10-19 14:44:37 -0300

    Update cron.py

--- cmcti/home/cron.py
+++ cmcti/home/cron.py
@@ -1,20 +1,21 @@
 from register.models import CustomUser
 from datetime import date
+def send_email():
 
+    user = CustomUser.objects.get(id=1)
+    
+    joined = user.date_joined
+    str_date = joined.strftime("%m:%d:%Y-%H:%M:%S")
 
-user = CustomUser.objects.get(id=1)
+    today = date.today()
+    str_today = today.strftime("%m:%d:%Y-%H:%M:%S")
 
-joined = user.date_joined
-str_date = joined.strftime("%m:%d:%Y-%H:%M:%S")
+    if str_today > str_date:
+        print('deu')
+    else:
+        print('nao deu')
 
-today = date.today()
-str_today = today.strftime("%m:%d:%Y-%H:%M:%S")
-
-if str_today > str_date:
-    print('deu')
-else:
-    print('nao deu')
-
+    return
 
 '''from datetime import date
 today = date.today()
