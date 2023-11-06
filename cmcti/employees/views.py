71813746d34a2fe39d466e3b4e85d40d8047b468
Lucca Torrezilha Soares <luccatsoares@gmail.com>
2023-11-06 08:24:57 -0300

    Update views.py

--- cmcti/employees/views.py
+++ cmcti/employees/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
+import requests
+from django.http import HttpResponse
+import json
 
 # Create your views here.
 @staff_member_required(login_url='/')
 @login_required(login_url='/entrar')
 def painelAdmin(request):
+
+    response = requests.get('http://127.0.0.1:8080/items/')
     
-    return render(request, 'pages/adminPanel.html')
+    #convert reponse data into json
+    api_data = response.json()
+    data_list = []
+    for objs in api_data:
+        data_list.append(objs)
+    
+    context = {
+        'products': data_list
+    }
+
+    if request.method == 'POST':
+        for objs in request:
+            print(objs)
+    return render(request, 'pages/adminPanel.html', context)
